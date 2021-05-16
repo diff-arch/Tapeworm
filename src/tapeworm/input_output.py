171360e5869a48a3994da2c694f6b34ddce6582a
diff-arch <10381015+diff-arch@users.noreply.github.com>
2021-05-16 16:59:05 +0200

    Update version to 1.0.1

--- src/tapeworm/input_output.py
+++ src/tapeworm/input_output.py
@@ -5,7 +5,7 @@ https://www.github.com/diff-arch/Tapeworm
 https://www.food4rhino.com/app/tapeworm
 @license GPL-3.0 <https://www.gnu.org/licenses/gpl.html>
 
-@version 1.0.0
+@version 1.0.1
 
 Input/Output
 """
